9b6a28b35c2b1fc3de62bdf8d41e2e719dadb72d
joseMarciano <marcianojosepaulo@gmail.com>
2021-02-06 17:51:20 -0300

    Adicionando seleção de cores

--- componentes/modal/modal.service.js
+++ componentes/modal/modal.service.js
@@ -1,4 +1,7 @@
 import Lembrete from '../../class/Lembrete.js';
+const ATRIBUTE_COLOR_SELECTED = 'data-color-selected';
+const ICON_SELECTED = 'fa-tint';
+const ICON_NOT_SELECTED = 'fa-tint-slash';
 
 const TEMPLATE_MODAL = `
     <section class="modal">
@@ -10,8 +13,21 @@ const TEMPLATE_MODAL = `
             <form>
                 <label for="description-modal" class="label__modal block">Descreva o lembrete:</label>
                 <textarea id="description-modal" class="text-area__modal" rows="3"></textarea>
-                <label class="label__modal block">Data:</label>
-                <input class="input__modal" type="datetime-local">
+                <div class="container-data-color__row">
+                    <div class="date--container__modal">
+                        <label class="label__modal block">Data:</label>
+                        <input class="input__modal" type="datetime-local">
+                    </div>
+                    <div class="color--container__modal">
+                        <label class="label__modal block">Cor:</label>
+                        <span class="color--options__modal" data-options-color>
+                            <i class="fa fa-tint select--color-icon icon" ${ATRIBUTE_COLOR_SELECTED}></i>
+                            <i class="fa fa-tint-slash select--color-icon icon"></i>
+                            <i class="fa fa-tint-slash select--color-icon icon"></i>
+                            <i class="fa fa-tint-slash select--color-icon icon"></i>
+                        </span>
+                    </div>
+                </div>
                 <div class="button-container__modal">
                     <button class="button--save__modal button">Salvar</button>
                     <button class="button--save-new__modal button">Salvar e adicionar novo</button>
@@ -23,8 +39,8 @@ const TEMPLATE_MODAL = `
 `;
 
 class ModalService {
-    
-    constructor(){
+
+    constructor() {
         this._lembrete = new Lembrete();
     }
 
@@ -34,8 +50,9 @@ class ModalService {
         modalContent.classList.add('modal-content', 'show-modal');
         modalContent.setAttribute('data-modal-content', '');
         modalContent.innerHTML = TEMPLATE_MODAL;
-        this._addEventListenerToCloseModal(modalContent, body);
         body.appendChild(modalContent);
+        this._addEventListenerToCloseModal(modalContent, body);
+        this._addEventListenerToAllBtnColors()
     }
 
     _closeModal = (modalContent, body) => {
@@ -51,6 +68,54 @@ class ModalService {
         const closeModalBtn = modalContent.querySelector('[data-close-modal-btn]');
         closeModalBtn.addEventListener('click', () => this._closeModal(modalContent, body));
     }
-    
+
+    _addEventListenerToAllBtnColors() {
+        const optionsColor = this._getOptionsColor();
+        optionsColor.forEach(color => {
+            color.addEventListener('click',this._selectColor);
+        });    
+    }
+
+    _getOptionsColor = () =>{
+        const modalContent = document.querySelector('[data-modal-content]');
+        const optionsParent  = modalContent.querySelector('[data-options-color]');
+        const optionsColor = [];
+        for (let i = 0; i < optionsParent.children.length; i++) {
+            optionsColor.push(optionsParent.children[i]);
+        }
+        return optionsColor;
+    }
+
+    _selectColor = (event) => {
+        this._removeIconSelected();
+        this._removeAttrSelected();
+        this._changeCurrentIconClicked(event.currentTarget);
+        this._addAttrSelectedInCurrent(event.currentTarget);
+    }
+
+    _removeIconSelected(){
+        const optionsColor = this._getOptionsColor();
+        optionsColor.forEach((color) =>{
+            color.classList.remove(ICON_SELECTED);
+            color.classList.add(ICON_NOT_SELECTED);
+        });
+
+    }
+
+    _removeAttrSelected(event){
+        const optionsColor = this._getOptionsColor();
+        optionsColor.forEach((color) =>{
+            color.removeAttribute(ATRIBUTE_COLOR_SELECTED);
+        });
+    }
+    _addAttrSelectedInCurrent(currentTarget){
+        currentTarget.setAttribute(ATRIBUTE_COLOR_SELECTED,'');
+    }
+
+    _changeCurrentIconClicked(currentTarget){
+        currentTarget.classList.remove(ICON_NOT_SELECTED);
+        currentTarget.classList.add(ICON_SELECTED);
+    }
+
 }
 export default ModalService;
